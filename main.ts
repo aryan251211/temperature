979c39dabb02b1f5cacf2076e2178a25a1545b53
aryan251211 <81881363+aryan251211@users.noreply.github.com>
2021-06-11 19:19:00 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (max_temperature))
+})
+input.onButtonPressed(Button.AB, function () {
+    max_temperature = input.temperature()
+    min_temperature = input.temperature()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (min_temperature))
+})
+let min_temperature = 0
+let max_temperature = 0
+max_temperature = input.temperature()
+min_temperature = input.temperature()
+basic.showNumber(input.temperature() * (1.8 + 32))
+basic.forever(function () {
+    if (input.temperature() < min_temperature) {
+        min_temperature = input.temperature()
+    }
+    if (input.temperature() > max_temperature) {
+        max_temperature = input.temperature()
+    }
+})
